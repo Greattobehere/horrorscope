e981efc966a1854840f6f82451f16ca591505a32
Greattobehere <127852983+Greattobehere@users.noreply.github.com>
2026-04-03 20:11:18 -0400

    Fix ReadingPageInner syntax error

--- app/reading/page.tsx
+++ app/reading/page.tsx
@@ -6,7 +6,7 @@ import { toPng } from "html-to-image";
 import PortraitCrossfade from "../components/PortraitCrossfade";
 import ShareCard from "../components/ShareCard";
 
-function ReadingPageInner
+export default function ReadingPage()
 export default function ReadingPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [currentPart, setCurrentPart] = useState(1);
