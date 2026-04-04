0abcb300a1fca9046dea9406f4b74e2c0ebcd869
Greattobehere <127852983+Greattobehere@users.noreply.github.com>
2026-04-03 20:21:09 -0400

    Remove duplicate export default

--- app/reading/page.tsx
+++ app/reading/page.tsx
@@ -6,7 +6,6 @@ import { toPng } from "html-to-image";
 import PortraitCrossfade from "../components/PortraitCrossfade";
 import ShareCard from "../components/ShareCard";
 
-export default function ReadingPage()
 export default function ReadingPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [currentPart, setCurrentPart] = useState(1);
